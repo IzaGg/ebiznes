42e69d3cf7b7722e8ef4c56fb9b6790f4ae6148c
Izabela Gorlinska <60105239+IzaGg@users.noreply.github.com>
2021-04-28 00:15:05 +0200

    Add files via upload

---
+++ sklep/app/controllers/OrderController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+
+@Singleton
+class OrderController {
+
+	def addOrder()={
+	}
+
+	def deleteOrder(id: String)={
+	}
+
+	def getOrder(id: String)={
+	}
+
+}
