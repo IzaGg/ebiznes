8b1f566f761ef6b7eb8a76b51ce527f421ccdc25
Izabela Gorlinska <60105239+IzaGg@users.noreply.github.com>
2021-04-28 00:31:09 +0200

    Add files via upload

---
+++ sklep/app/controllers/BrandController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+
+@Singleton
+class BrandController {
+
+	def addBrand()={
+	}
+
+	def deleteBrand(id: String)={
+	}
+
+	def getBrand(id: String)={
+	}
+
+}
