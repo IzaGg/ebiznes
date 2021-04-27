4623e285e9a2d9adf66ca4bfa7e8890d4be3d7fe
Izabela Gorlinska <60105239+IzaGg@users.noreply.github.com>
2021-04-28 01:02:02 +0200

    Add files via upload

---
+++ sklep/app/controllers/AuthController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+
+@Singleton
+class AuthController {
+
+	def login()={
+	}
+
+	def logout()={
+	}
+
+}
---
+++ sklep/app/controllers/BagController.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+
+@Singleton
+class BagController {
+
+	def addtoBag(ProductId: String)={
+	}
+
+	def deletefromBag(ProductId: String)={
+	}
+
+	def showBag()={
+	}
+
+}
